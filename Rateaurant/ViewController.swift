28474b1e9b92f17ab72e39fbe1fe12ba2ce8c911
Alessio Perrotti <alessio.perrotti.90@gmail.com>
2017-11-28 10:26:33 +0100

    Initial Commit

---
+++ Rateaurant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rateaurant
+//
+//  Created by Alessio Perrotti on 28/11/2017.
+//  Copyright © 2017 Alessio Perrotti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
